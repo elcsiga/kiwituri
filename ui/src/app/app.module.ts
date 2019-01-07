7023068277ff43a60791e2512d918f4c4c5d6c5f
Andras Kovacs <elcsiga@gmail.com>
2019-01-07 23:41:04 +0100

    upload form

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from "@angular/material";
-import { AppRoutingModule } from './/app-routing.module';
+import {
+  MatChipsModule,
+  MatIconModule, MatInputModule,
+  MatListModule, MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from "@angular/material";
+import { AppRoutingModule } from './app-routing.module';
 import { UploadViewComponent } from './views/upload-view/upload-view.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { HttpClientModule } from "@angular/common/http";
 import { UploadModule } from "./upload/upload.module";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
@@ -17,6 +28,8 @@ import { UploadModule } from "./upload/upload.module";
     HomeViewComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
@@ -25,7 +38,14 @@ import { UploadModule } from "./upload/upload.module";
     MatToolbarModule,
     MatListModule,
     AppRoutingModule,
-    UploadModule
+    UploadModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/views/upload-view/upload-view.component.ts
+++ ui/src/app/views/upload-view/upload-view.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {UploadService} from "../../upload/services/upload.service";
 
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {ElementRef, ViewChild} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '@angular/material';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-upload-view',
   templateUrl: './upload-view.component.html',
@@ -10,11 +17,127 @@ export class UploadViewComponent implements OnInit {
 
   report$ = this.uploadService.reports.value$;
 
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagCtrl = new FormControl();
+  filteredTags: Observable<string[]>;
+  tags: string[] = [];
+  allTags: string[] = [
+    'Póló',
+    'Kabát',
+    'Pulóver',
+    'Cipő',
+    'Nadrág',
+    'Csizma'
+  ];
+
+  sexControl = new FormControl('', [Validators.required]);
+  sexes = [
+    'Fiú',
+    'Lány',
+    'Unisex'
+  ];
+
+  sizeControl = new FormControl('', [Validators.required]);
+  sizes = [
+    '0-3 hónap',
+    '3-6 hónap',
+    '6-9 hónap',
+    '86-92',
+    '98-104',
+    '110-116',
+    '122-128',
+    '134-140',
+    '146-152',
+    '158-164',
+    'XS',
+    'S',
+    'M',
+    'L',
+    'XL',
+    'XXL',
+  ];
+
+  sizeEstimatedControl = new FormControl();
+
   constructor(
     private uploadService: UploadService) {
+
+    this.filteredTags = this.tagCtrl.valueChanges.pipe(
+      startWith(null),
+      map((tag: string | null) => tag ? this._filter(tag) : this.allTags.slice()));
+
   }
 
   ngOnInit() {
   }
+
+  @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+  add(event: MatChipInputEvent): void {
+    // Add tag only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      // Add our tag
+      if ((value || '').trim()) {
+        this.addTag(value.trim());
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.tagCtrl.setValue(null);
+    }
+  }
+
+  addTag(tag: string): void {
+    if (!this.isTagSelected(tag)) {
+      this.tags.push(tag);
+    }
+  }
+
+  isTagSelected(tag: string): boolean {
+    return this.tags.includes(tag);
+  }
+
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.push(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.tagCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTags.filter(zag => zag.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 }
