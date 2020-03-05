51395e7137ea149594e05d9f593548dfc356de2d
Andras Kovacs <elcsiga@gmail.com>
2020-03-05 22:39:12 +0100

    heroku1

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,12 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 
-import {db} from "./db/mysql";
+//import {db} from "./db/mysql";
 import {itemsRouter} from "./routers/items";
-import {uploadRouter} from "./routers/upload";
+//import {uploadRouter} from "./routers/upload";
 import {mailerRouter} from "./routers/mail";
 import {errorHandler, sendError} from "./utils/error";
-import {initAuth} from "./routers/authentication";
+//import {initAuth} from "./routers/authentication";
 import {testRouter} from "./routers/test";
 import {configRouter} from "./routers/config";
 
@@ -25,15 +25,15 @@ async function main() {
         next();
     });
 
-    initAuth(app);
+    //initAuth(app);
 
     //db
-    await db.connect();
+    //await db.connect();
 
     //modules
     app.use('/api/config', configRouter);
     app.use('/api/test', testRouter);
-    app.use('/api/upload', uploadRouter);
+    //app.use('/api/upload', uploadRouter);
     app.use('/api/items', itemsRouter);
     app.use('/api/mail', mailerRouter);
 
